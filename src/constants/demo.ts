350cbac3c91d14f2da5a9f3e3df640527f35dcf1
Camila Paladines <38636399+paladinescamila@users.noreply.github.com>
2026-09-07 15:34:49 -0500

    Show demo account

---
+++ src/constants/demo.ts
@@ -0,0 +1,111 @@
+export const DEMO_USER: User = {
+	name: 'Lisa',
+	id: 'QjemPXthw4cxCrL97KRnEftFoHi2',
+	email: 'lisa@mail.com',
+	photo:
+		'https://firebasestorage.googleapis.com/v0/b/mood-app-paladinescamila.firebasestorage.app/o/users%2FQjemPXthw4cxCrL97KRnEftFoHi2%2Fprofile.jpg?alt=media&token=24dc20c4-0e9b-4fb1-9eb6-0714ec776550',
+};
+
+export const DEMO_MOODS: MoodEntry[] = [
+	{
+		id: 'mtqgzdak-0ze909vj2nhi811wkgqxd0bcpfgf027sog44',
+		createdAt: '2026-04-07T10:00:00Z',
+		feelings: ['joyful', 'excited'],
+		mood: 'happy',
+		sleepHours: '7-8',
+		journalEntry:
+			'I feel really happy and excited today! I have a lot of energy and I am looking forward to the weekend.',
+	},
+	{
+		mood: 'happy',
+		sleepHours: '7-8',
+		createdAt: '2026-04-13T10:00:00Z',
+		journalEntry:
+			'I feel really happy and content today! I am grateful for all the good things in my life and I am looking forward to the future.',
+		feelings: ['grateful', 'content'],
+		id: 'mtqgzdak-2bwvfi7qbg9u3q9a1ayaamf2p7fjzb2iudk6',
+	},
+	{
+		journalEntry: 'I feel okay today. Nothing too exciting, but nothing too bad either.',
+		id: 'mtqgzdak-58g3mxr29atb2zv0m9h9tlauyzz7ku3xvk9x',
+		sleepHours: '5-6',
+		feelings: ['calm', 'peaceful'],
+		mood: 'neutral',
+		createdAt: '2026-04-06T10:00:00Z',
+	},
+	{
+		createdAt: '2026-04-12T10:00:00Z',
+		id: 'mtqgzdak-dkgjj8oi5j62tnpmw1g78a3ij9q6b4wgudiz',
+		feelings: ['calm', 'peaceful'],
+		journalEntry: 'I feel okay today. Nothing too exciting, but nothing too bad either.',
+		mood: 'neutral',
+		sleepHours: '7-8',
+	},
+	{
+		mood: 'sad',
+		sleepHours: '5-6',
+		feelings: ['anxious', 'lonely'],
+		createdAt: '2026-03-31T10:00:00Z',
+		journalEntry: 'I feel really sad today. Nothing seems to go right.',
+		id: 'mtqgzdak-dwxhqx63kij50k29loq1iky2rxwyqk9xf3dx',
+	},
+	{
+		journalEntry: 'I feel very sad and lonely today. I wish I had someone to talk to.',
+		createdAt: '2026-04-04T10:00:00Z',
+		mood: 'very-sad',
+		feelings: ['lonely', 'down'],
+		id: 'mtqgzdak-hxbrt48c95x031lw8l2m91bso8ntwkq3ktar',
+		sleepHours: '3-4',
+	},
+	{
+		sleepHours: '9+',
+		id: 'mtqgzdak-m1lrm3rwa79ulotjm0zdnthbpoplikutjkup',
+		feelings: ['anxious', 'excited'],
+		mood: 'very-happy',
+		journalEntry:
+			'I feel very happy and excited today! I have a lot of energy and I am looking forward to the weekend.',
+		createdAt: '2026-04-15T10:00:00Z',
+	},
+	{
+		journalEntry:
+			'I feel really sad and anxious today. I am worried about the future and I am feeling down.',
+		sleepHours: '3-4',
+		id: 'mtqgzdak-mi17479mmd89y9bvgzvlvgkpbmztdlam8607',
+		feelings: ['anxious', 'down'],
+		createdAt: '2026-04-10T10:00:00Z',
+		mood: 'sad',
+	},
+	{
+		sleepHours: '3-4',
+		mood: 'very-sad',
+		id: 'mtqgzdak-v04b171in03f0h56z8f23lgu8n2ilbgh81jg',
+		feelings: ['anxious', 'lonely'],
+		createdAt: '2026-04-14T10:00:00Z',
+		journalEntry: 'I feel very sad and lonely today. I wish I had someone to talk to.',
+	},
+	{
+		feelings: ['grateful', 'optimistic'],
+		mood: 'very-happy',
+		sleepHours: '9+',
+		createdAt: '2026-04-09T10:00:00Z',
+		id: 'mtqgzdak-xut6i4lb5bvcbzkwnn7elt0te8hihhnoo8u3',
+		journalEntry:
+			'I feel very happy and optimistic today! I am grateful for all the good things in my life and I am looking forward to the future.',
+	},
+	{
+		createdAt: '2026-04-02T10:00:00Z',
+		id: 'mtqgzdak-zwk3ly7vr1hniunx1znz0kdcpeftx7nl083r',
+		feelings: ['grateful', 'content'],
+		journalEntry: 'I had a great day today! I feel grateful for my friends and family.',
+		mood: 'happy',
+		sleepHours: '7-8',
+	},
+	// {
+	// 	sleepHours: '7-8',
+	// 	feelings: ['optimistic', 'content'],
+	// 	id: 'mtrholqo-q0gm7zaz1g18sb7ua556ouq1xjwmxi4h7g6d',
+	// 	createdAt: '2026-09-07T17:03:06.000Z',
+	// 	journalEntry: 'It has been a good day',
+	// 	mood: 'happy',
+	// },
+];
