f1af879905a2e3df05e82f7f9a04c190a04fd670
Camila Paladines <38636399+paladinescamila@users.noreply.github.com>
2026-09-06 19:16:51 -0500

    Start with sample data

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -2,6 +2,7 @@ import {create} from 'zustand';
 import {persist, createJSONStorage} from 'zustand/middleware';
 import {sortMoodsHistory} from '@/utils/sortMoodsHistory';
 import type {FirebaseUser} from '@/firebase/config';
+import {SAMPLE_MOODS_HISTORY, SAMPLE_USER} from '@/constants/sample';
 
 interface AppState {
 	authUser: FirebaseUser | null;
@@ -22,11 +23,11 @@ export const useAppStore = create<AppState>()(
 			authUser: null,
 			setAuthUser: (authUser) => set({authUser}),
 
-			user: null,
+			user: SAMPLE_USER,
 			setUser: (user) => set({user}),
 
 			// Moods history
-			moodsHistory: [],
+			moodsHistory: SAMPLE_MOODS_HISTORY,
 			setMoodsHistory: (moodsHistory) => set({moodsHistory: sortMoodsHistory(moodsHistory)}),
 			addMoodEntry: (moodEntry) =>
 				set(({moodsHistory}) => ({moodsHistory: sortMoodsHistory([...moodsHistory, moodEntry])})),
