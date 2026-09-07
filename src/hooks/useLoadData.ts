0d6a5327d7a819d51a1d0d2c9cfa8f94d3ef7ff5
Camila Paladines <38636399+paladinescamila@users.noreply.github.com>
2026-09-06 19:09:27 -0500

    Load sample user only on the first load of the app

--- src/hooks/useLoadData.ts
+++ src/hooks/useLoadData.ts
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react';
 import {useAppStore} from '@/stores/app';
 import {auth} from '@/firebase/config';
 import {getUser, getUserMoods} from '@/firebase/firestore';
+import {signIn} from '@/firebase/auth';
 
 export const useLoadData = () => {
 	const [loading, setLoading] = useState<boolean>(true);
@@ -9,6 +10,7 @@ export const useLoadData = () => {
 
 	const {setAuthUser, setUser, setMoodsHistory} = useAppStore();
 
+	// Load all user data (user info and moods history)
 	useEffect(() => {
 		const unsubscribe = auth.onAuthStateChanged(async (authUser) => {
 			setError(null);
@@ -39,6 +41,28 @@ export const useLoadData = () => {
 
 		return () => unsubscribe();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	// Log in the sample user only on the first load of the app
+	useEffect(() => {
+		const loginSampleUser = async () => {
+			const isFirstLoad = JSON.parse(localStorage.getItem('is-first-load') || 'true');
+
+			if (!isFirstLoad) return;
+
+			try {
+				const SAMPLE_USER_EMAIL = 'lisa@mail.com';
+				const SAMPLE_USER_PASSWORD = import.meta.env.VITE_SAMPLE_USER_PASSWORD;
+
+				await signIn(SAMPLE_USER_EMAIL, SAMPLE_USER_PASSWORD);
+			} catch (error) {
+				console.error('Error logging in sample user:', error);
+			}
+
+			localStorage.setItem('is-first-load', 'false');
+		};
+
+		loginSampleUser();
 	}, []);
 
 	return {loading, error};
